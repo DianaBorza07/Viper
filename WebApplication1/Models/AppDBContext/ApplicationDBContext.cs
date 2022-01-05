528e53418aac299e890691c58ad62bdb98b2a186
Andreea Ionescu <andreeaionescu0464@gmail.com>
2022-01-05 13:47:03 +0200

    view rent info

--- WebApplication1/Models/AppDBContext/ApplicationDBContext.cs
+++ WebApplication1/Models/AppDBContext/ApplicationDBContext.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
 
+
 namespace CarRental.Models.AppDBContext
 {
     public class ApplicationDBContext : IdentityDbContext<User>
